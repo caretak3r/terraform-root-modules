e2b6af52093ec273e84ad500e9725c1191bb77b5
Rohit Gudi <rgudi@cadent.tv>
2020-04-09 15:42:08 -0400

    Update main.tf

--- aws/tfstate-backend/main.tf
+++ aws/tfstate-backend/main.tf
@@ -1,6 +1,12 @@
 terraform {
-  required_version = ">= 0.12.24"
-
+  required_version = "~> 0.12.24"
+  required_providers {
+    aws        = "~> 2.56.0"
+    template   = "~> 2.0"
+    null       = "~> 2.0"
+    local      = "~> 1.3"
+    kubernetes = "~> 1.15"
+  }  
   backend "s3" {}
 }
 
