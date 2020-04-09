fab986733874bf64487f618f1f94a9c7d592cbaa
Rohit Gudi <rgudi@cadent.tv>
2020-04-09 15:40:38 -0400

    Update versions.tf

--- aws/eks-ec2-cluster/versions.tf
+++ aws/eks-ec2-cluster/versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = "~> 0.12.24"
 
   required_providers {
-    aws        = "~> 2.28.1"
+    aws        = "~> 2.56.0"
     template   = "~> 2.0"
     null       = "~> 2.0"
     local      = "~> 1.3"
