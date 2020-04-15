58cbd262c0ea07357537d9f697ac58e4d438c899
Rohit Gudi <rgudi@cadent.tv>
2020-04-15 09:03:54 -0400

    silence this warning, remove the quotes around "string"

--- aws/tfstate-backend/main.tf
+++ aws/tfstate-backend/main.tf
@@ -17,49 +17,49 @@ provider "aws" {
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Application or solution name (e.g. `app`)"
   default     = "terraform"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list
   default     = ["state"]
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS Region the S3 bucket should reside in"
   default     = "us-east-1"
 }
 
 variable "force_destroy" {
-  type        = "string"
+  type        = string
   description = "A boolean that indicates the S3 bucket can be destroyed even if it contains objects. These objects are not recoverable."
-  default     = "false"
+  default     = false
 }
 
 module "tfstate_backend" {
